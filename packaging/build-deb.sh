5fc5006ea39ae57c3f26154b7377bcd76785f9a0
Daren Loxley 2E0LXY <2e0lxy@aprsnet.uk>
2026-08-21 15:40:43 +0100

    Polish Debian package metadata

--- packaging/build-deb.sh
+++ packaging/build-deb.sh
@@ -22,7 +22,7 @@ cp -a "$project_root/src/ip_analyser" "$package_root/usr/lib/advanced-ip-analyse
 cp "$project_root/README.md" "$package_root/usr/share/doc/advanced-ip-analyser/README.md"
 cp "$project_root/CHANGELOG.md" "$package_root/usr/share/doc/advanced-ip-analyser/CHANGELOG.md"
 cp "$project_root/packaging/copyright" "$package_root/usr/share/doc/advanced-ip-analyser/copyright"
-cp "$project_root/packaging/changelog.Debian" "$package_root/usr/share/doc/advanced-ip-analyser/changelog.Debian"
+cp "$project_root/packaging/changelog.Debian" "$package_root/usr/share/doc/advanced-ip-analyser/changelog"
 cp "$project_root/packaging/advanced-ip-analyser.1" "$package_root/usr/share/man/man1/advanced-ip-analyser.1"
 cp "$project_root/src/ip_analyser/assets/advanced-ip-analyser.svg" \
    "$package_root/usr/share/icons/hicolor/scalable/apps/advanced-ip-analyser.svg"
@@ -32,8 +32,9 @@ chmod 0755 "$package_root/usr/bin/advanced-ip-analyser" "$package_root/usr/bin/a
 chmod 0755 "$package_root/DEBIAN"
 find "$package_root" -type d -exec chmod 0755 {} +
 find "$package_root" -type f ! -path '*/usr/bin/*' -exec chmod 0644 {} +
-gzip -9n "$package_root/usr/share/doc/advanced-ip-analyser/changelog.Debian"
+gzip -9n "$package_root/usr/share/doc/advanced-ip-analyser/changelog"
 gzip -9n "$package_root/usr/share/man/man1/advanced-ip-analyser.1"
+ln -s advanced-ip-analyser.1.gz "$package_root/usr/share/man/man1/advanced-ip-analyser-gui.1.gz"
 installed_size=$(du -sk "$package_root/usr" | cut -f1)
 sed -i "s/@VERSION@/$version/; s/@INSTALLED_SIZE@/$installed_size/" "$package_root/DEBIAN/control"
 
